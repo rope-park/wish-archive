02588b326f50cad7d1f981fb82fe505e4f98cee0
rope-park <kelly6789@naver.com>
2025-12-28 13:15:48 +0900

    Feat: Cloudinary를 통한 이미지 설정 최적화

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ]
+  }
 };
 
 export default nextConfig;

3410ea001230f2348ddba4477c772b6a844d60a1
rope-park <kelly6789@naver.com>
2026-01-29 16:06:39 +0900

    Feat: 인덱스 업데이트

--- src/components/apps/index.ts
+++ src/components/apps/index.ts
@@ -1,15 +1,19 @@
 /**
  * App 컴포넌트 인덱스
- * 
+ *
  * - app 컴포넌트들을 한 곳에서 import
  */
 
-export { default as MyWish } from './MyWish/MyWish';
+export { default as MyWish } from "./MyWish/MyWish";
 
-export { default as WishWorld } from './WishWorld/WishWorld';
+export { default as WishWorld } from "./WishWorld/WishWorld";
 
-export { default as WishArchive } from './WishArchive/WishArchive';
+export { default as WishArchive } from "./WishArchive/WishArchive";
 
-export { default as WishGallery } from './WishGallery/WishGallery';
+export { default as WishGallery } from "./WishGallery/WishGallery";
 
-export { default as Discography } from './Discography/Discography';
+export { default as Discography } from "./Discography/Discography";
+
+export { default as RecycleBin } from "./Recycle_Bin/RecycleBin";
+
+export { default as ToWish } from "./ToWish/ToWish";
